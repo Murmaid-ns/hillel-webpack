44f15d8187edb4b5a933caabc0d6af4cc6baa666
Slava <19uvs99@gmail.com>
2020-08-22 17:43:14 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import {sum} from "./math";
+
+function checkField(obj) {
+    return obj?.credentials?.name;
+}
+
+console.log(checkField({
+    credentials: {
+        name: 'adsd',
+    }
+}));
+
+function getArrField(arr) {
+    return arr?.[0]?.name;
+}
+
+console.log(sum(2,2));
+
+console.log(getArrField([]))
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        open: true,
+        overlay: true,
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.join(__dirname,'index.html')
+    })],
+};
